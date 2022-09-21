704490574be04cebccbe61bc86bdd016d10abd1f
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-09-21 15:47:25 +0200

    upload Image cloudinary

--- build/controllers/base/baseController.js
+++ build/controllers/base/baseController.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readAll = exports.remove = exports.update = exports.read = exports.create = void 0;
+const tslib_1 = require("tslib");
+const cloudinary_1 = tslib_1.__importDefault(require("../../utils/cloudinary"));
 const create = (model) => async (req, res, _next) => {
     const body = { ...req.body };
     try {
@@ -40,6 +42,15 @@ const update = (model) => async (req, res, _next) => {
     const { id } = req.params;
     const body = { ...req.body };
     try {
+        const fileImage = req.body?.profilePicture;
+        let uploadResponseCloudinary;
+        if (fileImage != undefined) {
+            uploadResponseCloudinary = await cloudinary_1.default.uploader.upload(fileImage, {
+                upload_preset: 'profile'
+            }, function (_error, result) { console.log(result); });
+            console.log(uploadResponseCloudinary);
+            body["profilePicture"] != undefined && uploadResponseCloudinary.url_secure;
+        }
         const doc = await model
             .findByIdAndUpdate(id, { $set: { ...body } }, { new: true })
             .lean()
--- server.ts
+++ server.ts
@@ -17,7 +17,8 @@ const app: Express = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 //Routers
 app.use("/auth", authRouter);
